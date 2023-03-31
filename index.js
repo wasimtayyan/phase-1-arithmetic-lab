e65604c76d5ed3fa5480064bf10de51a9dc69911
wasim altayyan <wasimtayyan@gmail.com>
2023-03-31 17:06:41 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1= 31
+const num2= 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*10)
+const num3 = 16
+const num4 = 6
+let mod = num3 % num4 
+let max = Math.max(2, 5, 6, 9, 12, 16, 20)
